c5ceb82a7b43f62d033d5d44e4602a5cf3950198
Flupster <esptidus@gmail.com>
2020-04-25 03:45:32 +0100

    added default useragent and env var to set up custom user agent

--- src/EsiClient.js
+++ src/EsiClient.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 const interceptors = require("./interceptors");
 const https = require("https");
 const httpsAgent = new https.Agent({ keepAlive: true });
+const package = require("../package.json");
 const { ConcurrencyManager } = require("axios-concurrency");
 const { Model } = require("objection");
 const Redis = require("ioredis");
@@ -11,16 +12,33 @@ const instance = axios.create({
   timeout: 11000,
   httpsAgent,
   model: require("./Token"),
-  redis: null
+  redis: null,
 });
 
-instance.interceptors.request.use(interceptors.request, interceptors.requestError);
-instance.interceptors.response.use(interceptors.response, interceptors.responseError);
+instance.defaults.headers.common["User-Agent"] =
+  process.env.AXIOS_USER_AGENT ||
+  `NodeESI v${package.version} - ${package.url}`;
+
+//Setup Interceptors
+instance.interceptors.request.use(
+  interceptors.request,
+  interceptors.requestError
+);
+
+instance.interceptors.response.use(
+  interceptors.response,
+  interceptors.responseError
+);
 
 //DB Binding
 instance.knex = knex => Model.knex(knex);
+
 //Concurrency Binding
-instance.manager = ConcurrencyManager(instance, process.env.ESI_CONCURRENCY || 10);
+instance.manager = ConcurrencyManager(
+  instance,
+  process.env.ESI_CONCURRENCY || 10
+);
+
 //Cache
 instance.cache = uri => (instance.defaults.redis = new Redis(uri));
 module.exports = instance;
